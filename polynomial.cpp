01afa31dc39f4ebe79f6265ad95f57115e43ddf2
ZKlack <xklack@outlook.sa>
2024-11-24 21:29:49 +0300

    implemented constructors

--- polynomial.cpp
+++ polynomial.cpp
@@ -1,1 +1,12 @@
 #include "polynomial.h"
+
+namespace ZK {
+	polynomial::polynomial()
+		:_terms() { }
+	polynomial::polynomial(const std::vector<real>& terms)
+		:_terms(terms) { }
+	polynomial::polynomial(const std::initializer_list<real>& terms)
+		:_terms(terms) { }
+	polynomial::polynomial(const polynomial& other)
+		:_terms(other._terms) { }
+}
