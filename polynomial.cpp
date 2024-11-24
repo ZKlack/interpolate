5adcc54ee1153793db91643c8f122273dc96752f
ZKlack <xklack@outlook.sa>
2024-11-24 21:38:22 +0300

    implemented evaluation operator for real numbers

--- polynomial.cpp
+++ polynomial.cpp
@@ -9,4 +9,17 @@ namespace ZK {
 		:_terms(terms) { }
 	polynomial::polynomial(const polynomial& other)
 		:_terms(other._terms) { }
+
+	real polynomial::operator()(const real x) const {
+		real result = 0;
+		for (num i = 0; i < _terms.size(); ++i)
+		{
+			real term = 1;
+			for (num j = 0; j < i; ++j)
+				term *= x;
+			term *= _terms[i];
+			result += term;
+		}
+		return result;
+	}
 }
