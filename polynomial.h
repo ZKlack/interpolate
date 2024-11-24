011e3a2fbdf74b0c2a96ee6f6e0197b5525c1be0
ZKlack <xklack@outlook.sa>
2024-11-24 21:13:45 +0300

    wrote all declarations of the polynomial class in polynomial.h

--- polynomial.h
+++ polynomial.h
@@ -7,5 +7,41 @@ namespace ZK {
 
 	class polynomial
 	{
+	private:
+		std::vector<real> _terms;
+	public:
+		polynomial();
+		polynomial(const std::vector<real>&);
+		polynomial(const std::initializer_list<real>&);
+		polynomial(const polynomial&);
+
+		real operator()(const real&) const;
+		polynomial operator()(const polynomial&) const;
+
+		polynomial operator+(const polynomial&) const;
+		polynomial operator-(const polynomial&) const;
+		polynomial operator*(const polynomial&) const;
+		polynomial operator+=(const polynomial&);
+		polynomial operator-=(const polynomial&);
+		polynomial operator*=(const polynomial&);
+
+		real operator+(real) const;
+		real operator-(real) const;
+		real operator*(real) const;
+		real operator/(real) const;
+		real operator+=(real);
+		real operator-=(real);
+		real operator*=(real);
+		real operator/=(real);
+
+		bool operator==(const polynomial&) const;
+		bool operator!=(const polynomial&) const;
+
+		real& operator[](num);
+		const real& operator[](num) const;
+		num size() const;
+
+		explicit operator std::string() const;
+		friend std::ostream& operator<<(std::ostream&, const polynomial&);
 	};
 }
