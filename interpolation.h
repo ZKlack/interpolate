972e3ee2b5d0fc993b061c14d9de03285ea552ac
ZKlack <xklack@outlook.sa>
2024-11-25 14:15:15 +0300

    wrote all declarations of the interpolation class in interpolation.h

--- interpolation.h
+++ interpolation.h
@@ -1,7 +1,27 @@
 #pragma once
 #include"polynomial.h"
+#include<map>
 namespace ZK {
 	class interpolation
 	{
+	private:
+		polynomial(*_interpolator)(const std::vector<std::pair<real, real> >&);
+		std::map<real,real> _points;
+		polynomial _func;
+	public:
+		interpolation(polynomial(*)(const std::vector<std::pair<real, real> >&));
+		interpolation(polynomial(*)(const std::vector<std::pair<real, real> >&), const std::map<real, real>&);
+		interpolation(const interpolation&);
+
+		real operator()(const real) const;
+		polynomial operator()(const polynomial&) const;
+
+		real get(const real) const;
+		real set(const real, const real);
+		real set(const std::pair<real, real>&);
+
+		std::vector<std::pair<real, real> > points() const;
+		num sample_size() const;
+		num degree() const;
 	};
 }
