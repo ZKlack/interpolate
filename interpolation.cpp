fa4d7a043649863f57c132c40bc85b0e82d7f2cf
ZKlack <xklack@outlook.sa>
2024-11-27 13:15:59 +0300

    wrote constructors

--- interpolation.cpp
+++ interpolation.cpp
@@ -1,5 +1,19 @@
 #include "interpolation.h"
 
 namespace ZK {
-
+	interpolation::interpolation(polynomial(*interpolator)(const std::vector<std::pair<real, real> >&))
+		: _interpolator(interpolator)
+		, _points()
+		, _func(_interpolator(std::vector<std::pair<real,real> >()))
+	{ }
+	interpolation::interpolation(polynomial(*interpolator)(const std::vector<std::pair<real, real> >&), const std::map<real, real>& points)
+		: _interpolator(interpolator)
+		, _points(points)
+		, _func(_interpolator(rewrap(_points)))
+	{ }
+	interpolation::interpolation(const interpolation& other)
+		: _interpolator(other._interpolator)
+		, _points(other._points)
+		, _func(other._func)
+	{ }
 }
