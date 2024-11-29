15fbe816f1e206bf7c820f3c06a94845b7c4a6d0
ZKlack <xklack@outlook.sa>
2024-11-29 16:57:55 +0300

    added newtons backward interpolation formula

--- methods.cpp
+++ methods.cpp
@@ -37,5 +37,36 @@ namespace ZK::methods {
 			}
 			return result;
 		}
+
+		polynomial newtons_backward(const std::vector<std::pair<real, real> >& points) {
+			if (points.size() == 0)
+				return 0;
+			if (points.size() == 1)
+				return points[0].second;
+			num sample = points.size();
+			real h = points[1].first - points[0].first;
+			for (num i = 2; i < sample; ++i)
+				if (points[i].first - points[i - 1].first != h)
+					throw std::invalid_argument("unequal intervals.");
+
+			std::vector<std::vector<real> > table(sample, std::vector<real>(sample, 0));
+			for (num j = 0; j < sample; ++j)
+				table[0][j] = points[j].second;
+			for (num i = 1; i < sample; ++i)
+				for (num j = i; j < sample; ++j)
+					table[i][j] = table[i - 1][j] - table[i - 1][j - 1];
+
+			polynomial u = { -points[sample - 1].first,1 };
+			u /= h;
+			polynomial result = 0;
+			for (num n = 0; n < sample; ++n)
+			{
+				polynomial term = table[n][sample - 1];
+				for (num i = 0; i < n; ++i)
+					term *= (u + i) / (n - i);
+				result += term;
+			}
+			return result;
+		}
 	}
 }
