d952774dea3f2fa1a20497ad80c62a847a7cf988
ZKlack <xklack@outlook.sa>
2024-12-01 15:48:37 +0300

    wrote args parser

--- main.cpp
+++ main.cpp
@@ -1,16 +1,53 @@
 #include"interpolation.h"
 #include"methods.h"
+#include <iostream>
 using namespace std;
 
 int ui_mode();
 int api_mode(string, bool, const vector<pair<long double, long double> >&);
 
+enum ERRORCODE {
+	UNKOWN_METHOD = 100,
+	NUMBER_TOO_LARGE
+};
+
 int main(int argc, char* argv[])
 {
 	if (argc == 1)
 		return ui_mode();
-	//parse params
-	return api_mode(/*pass good params*/);
+
+	string method = argv[1];
+	if (!ZK::methods::interpolation::methods.contains(method))
+	{
+		cerr << "Error: interpolation method unknown!\n";
+		for (auto itr : ZK::methods::interpolation::methods)
+			cerr << ' ' << itr.first << '\n';
+		return ERRORCODE::UNKOWN_METHOD;
+	}
+	bool human = string(argv[2]) == "-h";
+	vector<pair<long double, long double> > points;
+	points.reserve((argc >> 1) - 1);
+	for (long long i = 2 + human; i < argc; i += 2)
+	{
+		if (i + 1 == argc)
+		{
+			cerr << "Error: invalid arguments!\n interpolate <method> [-h] [<x0> <y0>] ... [<xn> <yn>]\n";
+			return 1;
+		}
+		try {
+			points.emplace_back(argv[i], argv[i + 1]);
+		}
+		catch (const invalid_argument& e) { // Catch invalid number conversion
+			cerr << "Error: invalid numeric value '" << argv[i] << "' or '" << argv[i + 1] << "'\n";
+			return 1;
+		}
+		catch (const out_of_range& e) { // Catch numbers that are too large
+			cerr << "Error: value out of range '" << argv[i] << "' or '" << argv[i + 1] << "'\n";
+			return ERRORCODE::NUMBER_TOO_LARGE;
+		}
+	}
+
+	return api_mode(method, human, points);
 }
 
 int ui_mode()
