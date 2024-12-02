5f1d77b259efc25d44d7fa52ae166eb9efade513
ZKlack <xklack@outlook.sa>
2024-12-02 11:55:38 +0300

    switched to static 32-bit build and made nessacary changes...

--- methods.h
+++ methods.h
@@ -2,13 +2,15 @@
 #include"polynomial.h"
 #include <map>
 
-namespace ZK::methods {
-	namespace interpolation {
-		extern const std::map<std::string, polynomial(*)(const std::vector<std::pair<real, real> >&)> methods;
+namespace ZK {
+	namespace methods {
+		namespace interpolation {
+			extern const std::map<std::string, polynomial(*)(const std::vector<std::pair<real, real> >&)> methods;
 
-		polynomial newtons_forward(const std::vector<std::pair<real, real> >&);
-		polynomial newtons_backward(const std::vector<std::pair<real, real> >&);
-		polynomial newtons_general_devided(const std::vector<std::pair<real, real> >&);
-		polynomial lagranges(const std::vector<std::pair<real, real> >&);
+			polynomial newtons_forward(const std::vector<std::pair<real, real> >&);
+			polynomial newtons_backward(const std::vector<std::pair<real, real> >&);
+			polynomial newtons_general_devided(const std::vector<std::pair<real, real> >&);
+			polynomial lagranges(const std::vector<std::pair<real, real> >&);
+		}
 	}
 }
